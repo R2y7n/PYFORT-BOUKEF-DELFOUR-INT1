fef357e70b5a95a8476180efb3add753ff2ab92f
anais <anais.delfour@efrei.net>
2024-12-06 11:09:44 +0100

    cc

---
+++ utility_functions.py
@@ -0,0 +1,4 @@
+def introduction():
+    print("Welcome to Fort Boyard!")
+    print("The player(s) must complete challenges to earn keys and unlock the treasure room.")
+    print("The aim is to collect three keys to access")
