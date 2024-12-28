56d001bfcf35b73f55bc2df18b0365e6a101bae1
anais <anais.delfour@efrei.net>
2024-12-28 20:50:16 +0100

    Normalement j'ai fini l'énigme du père fouras

--- pere_fouras_challenge.py
+++ pere_fouras_challenge.py
@@ -1,5 +1,14 @@
 import json
+import random
 
+def lowercase(str):
+    str_lower=''
+    for char in str:
+        if 'A' <= char <= 'Z':
+            str_lower += chr(ord(char) + 32)
+        else:
+            str_lower += char
+    return str_lower
 
 def load_riddles(file):
     with open(file, 'r') as file:
@@ -14,5 +23,28 @@ def load_riddles(file):
 
     return riddles
 
-
 print(load_riddles('data/PFRiddles.json'))
+
+
+def pere_fouras_riddles():
+    list_riddles = load_riddles('data/PFRiddles.json')
+    selected_riddle=random.choice(list_riddles)
+    print (selected_riddle['question'])
+    selected_riddle_lower = lowercase(selected_riddle['answer'])
+    attempt=3
+    while attempt > 0:
+        answer=input("Please enter your answer:")
+        answer_lower=lowercase(answer)
+        if answer_lower == selected_riddle_lower:
+            print("Your answer is correct !")
+            print("You win a key !")
+            return True
+        else:
+            print("Your answer is incorrect !")
+            attempt-=1
+            print(f"You have {attempt} attempts left")
+    print("You lose the key !")
+    print("The correct answer was:" , selected_riddle['answer'])
+    return False
+
+pere_fouras_riddles()
